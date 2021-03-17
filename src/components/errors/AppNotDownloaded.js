75f6d759d7e14007ba9b13f0f9ae6bc2b3edb2fa
leo polanco <leo.polanco.n@gmail.com>
2021-03-17 09:23:06 -0400

    Finished

--- src/components/errors/AppNotDownloaded.js
+++ src/components/errors/AppNotDownloaded.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getPage } from '../../actions/pages'
 
-// const AppNotDownloaded = ({ match, getPage, page: { page } }) => {
-const AppNotDownloaded = () => {
-  // useEffect(() => {
-  //   let id = match.params.id ? match.params.id : ''
-  //   getPage(id)
-  // }, [getPage, match])
+const AppNotDownloaded = ({ match, getPage, page: { page } }) => {
+  useEffect(() => {
+    let id = match.params.id ? match.params.id : ''
+    getPage(id)
+  }, [getPage, match])
 
   const divStyle = {
     textAlign: 'center'
@@ -17,8 +16,9 @@ const AppNotDownloaded = () => {
     <div style={divStyle}>
       <Link to='/adminDashboard'>Go to admin dashboard</Link>
       <h1>You don't have our App</h1>
-      {/* {page.pageName && <div>This page is for: {page.pageName}</div>} */}
       <br />
+      {page && <div>This page is for {page.pageName}</div>}
+      <br/>
       <a
         href='https://play.google.com/store/apps/details?id=com.instagram.android'
         target='_blank'
--- src/components/pages/ViewPage.js
+++ src/components/pages/ViewPage.js
@@ -12,27 +12,46 @@ const ViewPage = ({ match, getPage, page: { page } }) => {
   const divStyle = {
     textAlign: 'center'
   }
+
+  const copyTextToClipboard = (e) => {
+    var copyTextarea = document.querySelector('.js-copytextarea')
+    copyTextarea.value = e.target.value
+    copyTextarea.focus()
+    copyTextarea.select()
+    try {
+      document.execCommand('copy')
+    } catch (err) {
+      console.log('Oops, unable to copy')
+    }
+  }
+
   return page === null ? (
     <>Loading...</>
-  //Sometimes we navigate faster than the speed that the api return the pagename
+  ) : //Sometimes we navigate faster than the speed that the api return the pagename
   //We'll use a loading text while we wait
-  ) : id !== page.pageName ? (
+  id !== page.pageName ? (
     <>Loading...</>
   ) : (
     <div style={divStyle}>
       <Link to='/adminDashboard'>Go to admin dashboard</Link>
       <div>This page is for: {page.pageName}</div>
       <br />
-      <button type='button'>
-        <a
-          href='https://robtest.page.link/nPwh'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          Share this page
+      <button
+        type='button'
+        value={page.shareLink}
+        onClick={copyTextToClipboard}
+      >
+        Share this page
+      </button>
+      <div>
+        <textarea class='js-copytextarea' value={page.shareLink} />
+      </div>
+      <br />
+      <button>
+        <a href={page.shareLink} target='_blank' rel='noreferrer noopener'>
+          Open this page in our app
         </a>
       </button>
-      <div>{page.shareLink}</div>
     </div>
   )
 }
